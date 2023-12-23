17eda74da266d75b7e582f642e5b1b860ea2bfbf
Busayor123 <131611646+Busayor123@users.noreply.github.com>
2023-12-22 23:01:19 -0500

    Add files via upload

---
+++ MY HTML.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+yum update -y
+yum install -y httpd
+cd /var/www/html wget https://github.com/Busayor123/Busayor123/files/12009170/html.file.zip
+unzip html.file.zip 
+cp -r techmax-main/* /var/www/html/ 
+rm -rf html.file.zip 
+systemctl enable httpd
+systemctl start httpd
+ 
+
