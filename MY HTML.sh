bf1cefa19429793acbaa3f914a01c470b7e55605
Busayor123 <131611646+Busayor123@users.noreply.github.com>
2024-01-27 17:01:24 -0500

    Update MY HTML.sh

--- MY HTML.sh
+++ MY HTML.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 yum update -y
 yum install -y httpd
-cd /var/www/html wget https://github.com/Busayor123/Busayor123/files/12009170/html.file.zip
+cd /var/www/html 
+wget https://github.com/Busayor123/Busayor123/files/12009170/html.file.zip
 unzip html.file.zip 
 cp -r techmax-main/* /var/www/html/ 
 rm -rf html.file.zip 
