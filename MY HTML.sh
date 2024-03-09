d841b9a1358050735cfd9b57e5b32fd747fe23be
Busayor123 <131611646+Busayor123@users.noreply.github.com>
2024-03-09 08:55:52 -0500

    Update MY HTML.sh

--- MY HTML.sh
+++ MY HTML.sh
@@ -3,9 +3,9 @@ sudo yum update -y
 sudo yum install -y httpd
 cd /var/www/html 
 wget https://github.com/Busayor123/Html-website/raw/main/xmen-main.zip
-unzip html.file.zip 
+unzip xmen.main.zip 
 cp -r techmax-main/* /var/www/html/ 
-rm -rf html.file.zip 
+rm -rf xmen-main.zip 
 systemctl enable httpd
 systemctl start httpd
  
